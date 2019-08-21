a3b9c68ca29301fd951ecad98d7d6b267ab5eb5f
Jai Ko <just.jai.ko@gmail.com>
2019-08-21 10:13:57 -0700

    write specs for the parser

---
+++ test/util/parser.spec.js
@@ -0,0 +1,16 @@
+import parser from '~/src/util/parser';
+
+describe('parse decimal', () => {
+  it('parse num passed as string', () => {});
+  it('parse num passed as number', () => {});
+});
+
+describe('parse integers', () => {
+  it('parse num passed as string', () => {});
+  it('parse num passed as number', () => {});
+  it('parse num passed as BigInt', () => {});
+});
+
+describe('parse fraction', () => {
+  it('separates integer part from fraction part', () => {});
+});
