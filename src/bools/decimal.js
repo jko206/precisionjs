26214197c3f1f27a354917746fe05cb4927529b4
J Ko <jko@amazon.com>
2020-03-15 19:41:03 -0700

    modify decimal definitions

--- src/bools/decimal.js
+++ src/bools/decimal.js
@@ -1,5 +1,4 @@
-const isRegularDecimal = n => /^([1-9]\d*|0)\.\d+$/.test(n);
+export const isTerminatingDec = n => /^([1-9]\d*|0)?\.\d+$/.test(n);
 
-const isRepeatingDecimal = n => /^(\d+\.)?\d+\.{3}\d+$/.test(n);
+export const isRepeatingDec = n => /^(\d+\.)?\d+\.{3}\d+$/.test(n);
 
-export default n => isRegularDecimal(n) || isRepeatingDecimal(n);
