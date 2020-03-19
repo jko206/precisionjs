09e9c301efae2126b0ec17c53013c28b925d0c9b
J Ko <jko@amazon.com>
2020-03-18 22:18:37 -0700

    write natural number tester

---
+++ src/bools/natural.js
@@ -0,0 +1,3 @@
+import isZero from './zero'
+
+export default n => !isZero(n) && /^\d+(\.?0*)?$/.test(n)
---
+++ src/bools/natural.test.js
@@ -0,0 +1,16 @@
+import isNatural from './natural'
+
+describe('natural.js', () => {
+  it('correctly identifies valid natural', () => {
+    expect(isNatural('1234567890123456789012345678901234567890')).toBe(true)
+    expect(isNatural('12345')).toBe(true)
+    expect(isNatural('1')).toBe(true)
+    expect(isNatural('1.')).toBe(true)
+    expect(isNatural('1.0')).toBe(true)
+    expect(isNatural('1.00000000000000')).toBe(true)
+
+    expect(isNatural('0')).toBe(false)
+    expect(isNatural('000')).toBe(false)
+    expect(isNatural('1.2')).toBe(false)
+  })
+});
