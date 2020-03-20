c83803240e36acee08d2a2d694d70d24b60e4c61
J Ko <jko@amazon.com>
2020-03-19 17:23:06 -0700

    WIP: write spec for parsing fraction

---
+++ src/parser/fraction.test.js
@@ -0,0 +1,27 @@
+import { MAX_ARNUM_DIGIT } from '../constants'
+import { terminatingDec, repeatingDec } from './decimal'
+
+describe('decimal.js', () => {
+  it('parses terminating decimal', () => {
+    expect(terminatingDec('1/10')).toEqual({
+      numerator: [1],
+      denominator: [10],
+    })
+    expect(terminatingDec('10/10')).toEqual({
+      numerator: [1],
+      denominator: [1],
+    })
+    expect(terminatingDec('10/1')).toEqual({
+      numerator: [10],
+      denominator: [1],
+    })
+    expect(terminatingDec('1000000/1')).toEqual({
+      numerator: [1000000],
+      denominator: [1],
+    })
+    expect(terminatingDec('10000000/1')).toEqual({
+      numerator: [0, 1],
+      denominator: [1],
+    })
+  })
+})
