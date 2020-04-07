1309a512ebb58c09f2e56cc3fbada7089d988acd
J Ko <jko@amazon.com>
2020-04-06 22:42:06 -0700

    write plain number and add explanation

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,10 @@
-export const MAX_ARNUM_DIGIT = (() => {
-  const ceil = Number.MAX_SAFE_INTEGER ** 0.5;
-  let n = 10;
-  while (n * 10 < ceil) n *= 10;
-  return n;
-})();
+/**
+ *  MAX_ARNUM_DIGIT is the highest power of 10 that is less than the square root of Number.MAX_SAFE_INTEGER.
+ *  This way, even when 
+ */
+
+// TODO: Replace MAX_ARNUM_DIGIT with ARNUM_BASE everywhere
+export const MAX_ARNUM_DIGIT = 10000000 // 10,000,000
 
 export const LOG_10_ARNUM_BASE = Math.log10(MAX_ARNUM_DIGIT)
 
