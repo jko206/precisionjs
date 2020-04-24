7492ebc4697d69c439151fa1faa1d0ee22154a9c
J Ko <jko@amazon.com>
2020-04-24 09:20:34 -0700

    add types

---
+++ src/bools/types.ts
@@ -0,0 +1,14 @@
+type simple = number
+
+type arnum = number[]
+
+type integer = {
+    positivity: -1 | 0 | 1,
+    value: arnum,
+}
+
+type rational = {
+    positivity: -1 | 0 | 1,
+    numerator: arnum,
+    denominator: arnum,
+}
