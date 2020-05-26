48000ac13ccdb30c45a8a66fbbd22bd443e6d99d
J Ko <jko@amazon.com>
2020-05-26 16:55:44 -0700

    improve documentation

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,9 @@
 /**
  *  ARNUM_BASE is the highest power of 10 that is less than the square root of Number.MAX_SAFE_INTEGER.
- *  This way, even when 
  */
-
 export const ARNUM_BASE = 10000000 // 10,000,000
 
-export const LOG_10_ARNUM_BASE = Math.log10(ARNUM_BASE)
+export const LOG_10_ARNUM_BASE = Math.log10(ARNUM_BASE) // 7
 
 export const ZERO = {
   numerator: [0],
