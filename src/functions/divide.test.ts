207592b0b96c11de6579441b8003d9eb03857534
J Ko <jko@amazon.com>
2020-05-26 17:27:24 -0700

    scaffold divide fn.

---
+++ src/functions/divide.test.ts
@@ -0,0 +1,5 @@
+import { testDivide } from './divide'
+
+describe('divide.ts', () => {
+  it('divides', () => {})
+})
---
+++ src/functions/divide.ts
@@ -0,0 +1,9 @@
+import { arnum } from '../bools/types'
+
+const divideArnums = (n1: arnum, n2: arnum): { quotient: arnum; remainder?: arnum } => {
+  return {
+    quotient: [],
+  }
+}
+
+export const testDivide = (n1: number, n2: number): number => Math.floor(n1 / n2)
