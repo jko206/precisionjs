5f1e8609ecf36ced563bd95dd2d390bb794b288e
J Ko <jko@amazon.com>
2020-05-26 17:27:01 -0700

    setup for testing TS files

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  transform: {
+    '.(js|ts|tsx|jsx)': 'ts-jest',
+  },
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  testRegex: '.*.test.(tsx|ts)$',
+  moduleDirectories: ['./node_modules'],
+  runner: 'jest-serial-runner',
+}
