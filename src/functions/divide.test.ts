6adef30c2c0eb4595b3a1dc338cf9671de51b86b
J Ko <jko@amazon.com>
2020-05-27 08:44:32 -0700

    divide WIP

--- src/functions/divide.test.ts
+++ src/functions/divide.test.ts
@@ -1,5 +1,7 @@
 import { arnum } from '../bools/types'
-import { divideArnums } from './divide'
+import { divideArnums, double } from './divide'
+import { ARNUM_BASE } from '../constants'
+import { multiplyArnums } from './multiply'
 
 type testCaseInput = {
   input: [arnum, arnum]
@@ -11,9 +13,60 @@ type testCaseInput = {
 
 const testCases: testCaseInput[] = [
   {
+    input: [[5], [5]],
+    expected: {
+      quotient: [1],
+    },
+  },
+  {
+    input: [[6], [5]],
+    expected: {
+      quotient: [1],
+      remainder: [1],
+    },
+  },
+  {
+    input: [
+      [0, 0, 0, 0, 5],
+      [0, 0, 0, 0, 5],
+    ],
+    expected: {
+      quotient: [1],
+    },
+  },
+  {
+    input: [
+      [0, 0, 0, 0, 6],
+      [0, 0, 0, 0, 5],
+    ],
+    expected: {
+      quotient: [1],
+      remainder: [0, 0, 0, 0, 1],
+    },
+  },
+  {
     input: [[200], [5]],
     expected: {
       quotient: [40],
+    },
+  },
+  {
+    input: [[201], [5]],
+    expected: {
+      quotient: [40],
+      remainder: [1],
+    },
+  },
+  // {
+  //   input: [[0, 20000], [5]],
+  //   expected: {
+  //     quotient: [0, 4000],
+  //   },
+  // },
+  {
+    input: [[1024], [2]],
+    expected: {
+      quotient: [512],
     },
   },
 ]
@@ -23,6 +76,49 @@ describe('divide.ts', () => {
     test(`Test #${index + 1}`, () => {
       const output = divideArnums(...testCase.input)
       expect(output).toEqual(testCase.expected)
+    })
+  })
+})
+
+type testCaseInput2 = {
+  input: arnum
+  expected: arnum
+}
+const testCases2: testCaseInput2[] = [
+  {
+    input: [1],
+    expected: [2],
+  },
+  {
+    input: [0, 1],
+    expected: [0, 2],
+  },
+  {
+    input: [ARNUM_BASE / 2],
+    expected: [0, 1],
+  },
+  {
+    input: [0, 0, 0, ARNUM_BASE / 2],
+    expected: [0, 0, 0, 0, 1],
+  },
+]
+
+describe('double arnums', () => {
+  testCases2.forEach(({ input, expected }, index) => {
+    test(`Test #${index + 1}`, () => {
+      const output = double(input)
+      expect(output).toEqual(expected)
+    })
+  })
+})
+
+describe('double arnums, against multiplyArnums()', () => {
+  testCases2.forEach(({ input }, index) => {
+    test(`Test #${index + 1}`, () => {
+      const output = double(input)
+      const output2 = multiplyArnums(input, [2])
+      index === 2 && console.log(output2)
+      expect(output).toEqual(output2)
     })
   })
 })
--- src/functions/divide.ts
+++ src/functions/divide.ts
@@ -1,18 +1,57 @@
 import { arnum } from '../bools/types'
-import { comparator } from './comparator'
+import { compareArnum } from './comparator'
+import { multiplyArnums } from './multiply'
+import { subtractArnums } from './subtract'
+import { addArnums } from './add'
+import { ARNUM_BASE } from '../constants'
+
+const isZero = (num: arnum) => num.length === 1 && num[0] === 0
+type doubleFactorValue = [arnum, arnum]
 
 export const divideArnums = (
   dividend: arnum,
   divisor: arnum,
 ): { quotient: arnum; remainder?: arnum } => {
-  const geometricSeriesOfTwo: arnum[] = []
-  let double = [...divisor]
-  comparator([1], [0])
-  // while (comparator(double, dividend) < 1) {
-  //   geometricSeriesOfTwo.push(double)
-  //   double =
-  // }
-  return {
-    quotient: [dividend[0] / divisor[0]],
+  const geometricSeriesOfTwo: doubleFactorValue[] = []
+  let doubleValue = [...divisor] as arnum
+  let doubleFactor = [1]
+  while (compareArnum(doubleValue, dividend) < 1) {
+    geometricSeriesOfTwo.push([doubleFactor, doubleValue])
+    doubleValue = multiplyArnums(doubleValue, [2])
+    doubleFactor = multiplyArnums(doubleFactor, [2])
   }
+  const toSum: arnum[] = []
+  while (geometricSeriesOfTwo.length) {
+    ;[doubleFactor, doubleValue] = geometricSeriesOfTwo.pop() as doubleFactorValue
+    if (compareArnum(dividend, doubleValue) > -1) {
+      toSum.push(doubleFactor)
+      dividend = subtractArnums(dividend, doubleValue)
+      if (isZero(dividend)) break
+    }
+  }
+
+  return {
+    quotient: addArnums(...toSum),
+    ...(!isZero(dividend) && { remainder: dividend }),
+  }
+}
+
+export const double = (num: arnum) => {
+  let toCarry = 0
+  num = num
+    .map((n) => n * 2)
+    .map((n) => {
+      const val = n + toCarry
+      toCarry = Math.floor(val / ARNUM_BASE)
+      const newVal = val % ARNUM_BASE
+
+      return val % ARNUM_BASE
+    })
+  if (toCarry) num.push(toCarry)
+
+  return num
+}
+
+export const halve = (num: arnum) => {
+  return num.map((n) => n / 2).map((n) => {})
 }
