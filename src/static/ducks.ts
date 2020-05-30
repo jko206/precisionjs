6b80747f9f1707a450a02f753ba5ee88822e1e18
J Ko <jko@amazon.com>
2020-05-29 22:42:29 -0700

    remove type def

--- src/static/ducks.ts
+++ src/static/ducks.ts
@@ -8,7 +8,7 @@ export type stringOption = {
   roundTo: 'repeat' | number // 2 => 10^2 => round to 100th, -2 => 0.01 => round to 0.01th
 }
 
-export type validArgs = string | number | number[] | RationalNumber | Natural
+export type validArgs = string | number | RationalNumber | Natural
 
 export type arnum = number[]
 
