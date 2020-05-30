efcb03426dd398c743fa539671e8806ed75e7f19
J Ko <jko@amazon.com>
2020-05-29 22:08:00 -0700

    fix import

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@
 export { addArnums } from './functions/add/index'
 export { subtractArnums } from './functions/subtract'
 export { multiplyArnums } from './functions/multiply'
-export { default as getArnum } from './numbers/natural/arnum'
+export { default as getArnum } from './numbers/arnum'
