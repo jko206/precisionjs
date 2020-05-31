22edd1e2858178def3f5728fdd1df582c3359077
J Ko <jko@amazon.com>
2020-05-30 17:32:37 -0700

    improve definition

--- src/definitions/zero.ts
+++ src/definitions/zero.ts
@@ -1,2 +1,11 @@
-export default (n: number | string) =>
-  n === 0 || (/^\-?0*\.?0*$/.test(n as string) && n !== '.' && n !== '-.' && n !== '-')
+import { arnum, validArgs } from '../static/ducks'
+import { isArray } from 'util'
+
+export const isArnumZero = (n: arnum) => isArray(n) && (!n.length || (n.length === 1 && n[0] === 0))
+export const isStringZero = (n: string) =>
+  /^\-?0*\.?0*$/.test(n as string) && n !== '.' && n !== '-.' && n !== '-'
+
+export const isZero = (n: string | number | arnum) =>
+  n === 0 || isStringZero(n as string) || isArnumZero(n as arnum)
+
+export default isZero
