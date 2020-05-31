ce5cb9a1c8a3c52113abc28870692198a605e769
J Ko <jko@amazon.com>
2020-05-30 22:42:46 -0700

    name the default export

--- src/definitions/negative.ts
+++ src/definitions/negative.ts
@@ -1,5 +1,7 @@
-export default (n: number): boolean => {
+const isNegative = (n: number): boolean => {
   n = Number(n)
   if (Number.isNaN(n)) throw new Error(`Invalid argument: ${n} is NaN`)
   return n < 0
 }
+
+export default isNegative
