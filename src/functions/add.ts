9cb48a6e1dc16587f04fc101b929dc6060c19034
J Ko <jko@amazon.com>
2020-05-30 22:45:06 -0700

    WIP

--- src/functions/add.ts
+++ src/functions/add.ts
@@ -3,6 +3,7 @@ import { ARNUM_BASE } from '../static/constants'
 import { validArgs, arnum } from '../static/ducks'
 import RN from '../numbers/rational'
 import multiplyArnums from './multiply'
+import RationalNumber from '../numbers/rational'
 
 const carryToNextDigit = (arnum: arnum): arnum => {
   let toCarry = 0
@@ -36,21 +37,21 @@ export const addTwoArnums = (a: arnum, b: arnum): arnum => {
 export const addArnums = (...nums: arnum[]): arnum => nums.reduce(addTwoArnums, [0])
 
 export const addRationalNumbers = (num1: RN, num2: RN): RN => {
-  if (num1.isZero()) return new RN(num2)
-  if (num2.isZero()) return new RN(num1)
+  if (num1.isZero()) return num2.clone()
+  if (num2.isZero()) return num1.clone()
 
   const { numer: n1, denom: d1 } = num1
   const { numer: n2, denom: d2 } = num2
 
   const numer = multiplyArnums(n1.digits, n2.digits)
   const denom = multiplyArnums(d1.digits, d2.digits)
-
-  return new RN(0)
+  // TODO: must consider positiviity before adding or subtracting
+  return new RationalNumber(numer, denom)
 }
 
-const add = (estimate: true | validArgs, ...nums: validArgs[]): number | RN =>
+const add = (estimate: true | RationalNumber, ...nums: RationalNumber[]): number | RN =>
   estimate
     ? nums.map((n) => Number(n)).reduce((a, b) => a + b, 0)
-    : [estimate, ...nums].map((n) => new RN(n)).reduce(addRationalNumbers, new RN(0))
+    : [estimate, ...nums].reduce(addRationalNumbers, new RationalNumber([0]))
 
 export default add
---
+++ src/parser/index.ts
@@ -0,0 +1,46 @@
+import RationalNumber from '../numbers/rational'
+import isZero from '../definitions/zero'
+import { isRepeatingDec, isTerminatingDec } from '../definitions/decimal'
+import { terminatingDec, repeatingDec } from './decimal'
+import add from '../functions/add'
+import isSciNot from '../definitions/sci-not'
+import isNatural from '../definitions/natural'
+import getArnum from '../numbers/arnum'
+import { processNegativeNumber } from '../util/misc'
+
+const parser = (num: number | string): RationalNumber => {
+  const processed = processNegativeNumber(`${num}`)
+  num = processed.num
+  const { isNegative } = processed
+  if (isZero(num)) {
+    return new RationalNumber([0], [1])
+  }
+
+  if (isSciNot(num)) {
+    return new RationalNumber([1], [1], isNegative) // TODO: multiply by scinot part
+  }
+
+  if (isRepeatingDec(num)) {
+    const { repDecArnum, termDecArnum } = repeatingDec(num)
+    const { numerator: repDecNumer, denominator: repDecDenom } = repDecArnum
+    const { numerator: termDecNumer, denominator: termDecDenom } = termDecArnum
+
+    const rn = add(
+      new RationalNumber(repDecNumer, repDecDenom),
+      new RationalNumber(termDecNumer, termDecDenom),
+    ) as RationalNumber
+    rn.positivity = isNegative ? -1 : 1
+
+    return rn
+  } else if (isTerminatingDec(num)) {
+    const { numerator, denominator } = terminatingDec(num)
+
+    return new RationalNumber(numerator, denominator, isNegative)
+  } else if (isNatural(num)) {
+    return new RationalNumber(getArnum(num), [1], isNegative)
+  } else {
+    throw new Error(`Invalid argument: ${num}`)
+  }
+}
+
+export default parser
